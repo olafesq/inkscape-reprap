a86c893fc236e3e5902964eda7d299211d5b1dde
olafesq <olafesq@gmail.com>
2014-04-09 00:57:57 +0300

    Update context.py

--- src/reprap/context.py
+++ src/reprap/context.py
@@ -21,64 +21,64 @@ class GCodeContext:
       self.last = None
 
       self.preamble = [
-        "(Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
-        "( %s )" % " ".join(sys.argv),
-        "G21 (metric ftw)",
-        "G90 (absolute mode)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        ";(Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
+        ";( %s )" % " ".join(sys.argv),
+        "G21 ;(metric ftw)",
+        "G90 ;(absolute mode)",
+        "G92 X%.2f Y%.2f Z%.2f ;(you are here)" % (self.x_home, self.y_home, self.z_height),
         ""
       ]
 
       self.postscript = [
         "",
-				"(end of print job)",
-				"G1 Z%0.2F (pen up)" % self.pen_up_height,
-				"G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
+				";(end of print job)",
+				"G1 Z%0.2F ;(pen up)" % self.pen_up_height,
+				"G4 P%d ;(wait %dms)" % (self.stop_delay, self.stop_delay),
 				"G1 X0 Y0 F%0.2F" % self.xy_feedrate,
-				"G1 Z%0.2F F%0.2F (go up to finished level)" % (self.finished_height, self.z_feedrate),
-				"G1 X%0.2F Y%0.2F F%0.2F (go home)" % (self.x_home, self.y_home, self.xy_feedrate),
-				"M18 (drives off)",
+				"G1 Z%0.2F F%0.2F ;(go up to finished level)" % (self.finished_height, self.z_feedrate),
+				"G1 X%0.2F Y%0.2F F%0.2F ;(go home)" % (self.x_home, self.y_home, self.xy_feedrate),
+				"M18 ;(drives off)",
       ]
 
       self.registration = [
-        "G1 Z%d (pen down)" % (self.z_height),
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "G1 Z%d (pen up)" % (self.pen_up_height),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "M18 (disengage drives)",
-        "M01 (Was registration test successful?)",
-        "M17 (engage drives if YES, and continue)",
+        "G1 Z%d ;(pen down)" % (self.z_height),
+        "G4 P%d ;(wait %dms)" % (self.start_delay, self.start_delay),
+        "G1 Z%d ;(pen up)" % (self.pen_up_height),
+        "G4 P%d ;(wait %dms)" % (self.stop_delay, self.stop_delay),
+        "M18 ;(disengage drives)",
+        "M01 ;(Was registration test successful?)",
+        "M17 ;(engage drives if YES, and continue)",
         ""
       ]
 
       self.sheet_header = [
-        "(start of sheet header)",
-        "G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        ";(start of sheet header)",
+        "G92 X%.2f Y%.2f Z%.2f ;(you are here)" % (self.x_home, self.y_home, self.z_height),
       ]
       if self.register_pen == 'true':
         self.sheet_header.extend(self.registration)
-      self.sheet_header.append("(end of sheet header)")
+      self.sheet_header.append(";(end of sheet header)")
 
       self.sheet_footer = [
-        "(Start of sheet footer.)",
-        "G1 Z%d (pen up)" % (self.pen_up_height),
-        "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay),
-        "G91 (relative mode)",
+        ";(Start of sheet footer.)",
+        "G1 Z%d ;(pen up)" % (self.pen_up_height),
+        "G4 P%d ;(wait %dms)" % (self.stop_delay, self.stop_delay),
+        "G91 ;(relative mode)",
         "G0 Z15 F%0.2f" % (self.z_feedrate),
-        "G90 (absolute mode)",
+        "G90 ;(absolute mode)",
         "G0 X%0.2f Y%0.2f F%0.2f" % (self.x_home, self.y_home, self.xy_feedrate),
-        "M01 (Have you retrieved the print?)",
-        "(machine halts until 'okay')",
-        "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay),
-        "G91 (relative mode)",
-        "G0 Z-15 F%0.2f (return to start position of current sheet)" % (self.z_feedrate),
-        "G0 Z-0.01 F%0.2f (move down one sheet)" % (self.z_feedrate),
-        "G90 (absolute mode)",
-        "M18 (disengage drives)",
-        "(End of sheet footer)",
+        "M01 ;(Have you retrieved the print?)",
+        ";(machine halts until 'okay')",
+        "G4 P%d ;(wait %dms)" % (self.start_delay, self.start_delay),
+        "G91 ;(relative mode)",
+        "G0 Z-15 F%0.2f ;(return to start position of current sheet)" % (self.z_feedrate),
+        "G0 Z-0.01 F%0.2f ;(move down one sheet)" % (self.z_feedrate),
+        "G90 ;(absolute mode)",
+        "M18 ;(disengage drives)",
+        ";(End of sheet footer)",
       ]
 
-      self.loop_forever = [ "M30 (Plot again?)" ]
+      self.loop_forever = [ "M30 ;(Plot again?)" ]
 
       self.codes = []
 
@@ -109,13 +109,13 @@ class GCodeContext:
             print line
 
     def start(self):
-      self.codes.append("G1 Z%0.2F (pen down)" % self.z_height)
-      self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
+      self.codes.append("G1 Z%0.2F ;(pen down)" % self.z_height)
+      self.codes.append("G4 P%d ;(wait %dms)" % (self.start_delay, self.start_delay))
       self.drawing = True
 
     def stop(self):
-      self.codes.append("G1 Z%0.2F (pen up)" % self.pen_up_height)
-      self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
+      self.codes.append("G1 Z%0.2F ;(pen up)" % self.pen_up_height)
+      self.codes.append("G4 P%d ;(wait %dms)" % (self.stop_delay, self.stop_delay))
       self.drawing = False
 
     def go_to_point(self, x, y, stop=False):
@@ -125,8 +125,8 @@ class GCodeContext:
         return
       else:
         if self.drawing: 
-            self.codes.append("G1 Z%0.2F (pen up)" % self.pen_up_height) 
-            self.codes.append("G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay))
+            self.codes.append("G1 Z%0.2F ;(pen up)" % self.pen_up_height) 
+            self.codes.append("G4 P%d ;(wait %dms)" % (self.stop_delay, self.stop_delay))
             self.drawing = False
         self.codes.append("G1 X%.2f Y%.2f F%.2f" % (x,y, self.xy_feedrate))
       self.last = (x,y)
@@ -138,8 +138,8 @@ class GCodeContext:
         return
       else:
         if self.drawing == False:
-            self.codes.append("G1 Z%0.2F (pen down)" % self.pen_up_height)
-            self.codes.append("G4 P%d (wait %dms)" % (self.start_delay, self.start_delay))
+            self.codes.append("G1 Z%0.2F ;(pen down)" % self.pen_up_height)
+            self.codes.append("G4 P%d ;(wait %dms)" % (self.start_delay, self.start_delay))
             self.drawing = True
         self.codes.append("G1 X%0.2f Y%0.2f F%0.2f" % (x,y, self.xy_feedrate))
       self.last = (x,y)
